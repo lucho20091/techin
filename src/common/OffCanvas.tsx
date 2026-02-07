b662905da7422658aaf86a0940d2f69e559c4a75
lucho20091 <124323772+lucho20091@users.noreply.github.com>
2026-02-07 18:00:10 -0500

    new changes

--- src/common/OffCanvas.tsx
+++ src/common/OffCanvas.tsx
@@ -1,68 +1,31 @@
- 
 import { Link } from "react-router-dom";
 import { useState } from "react";
 
 const mobile_menu_data = [
   {
-    title: "Demo",
-    has_dropdown: true,
-    link: "#",
-    submenu: [
-      { title: "Home 01", link: "/" },
-      { title: "Home 02", link: "/home-2" },
-      { title: "Home 03", link: "/home-3" },
-    ],
+    title: "Home",
+    has_dropdown: false,
+    link: "/",
   },
   {
-    title: "Pages",
-    has_dropdown: true,
-    link: "#",
-    submenu: [
-      { title: "About Us", link: "/about-us" },
-      { title: "Pricing", link: "/pricing" },
-      { title: "Blog", link: "/blog" },
-      { title: "Blog grid", link: "/blog-grid" },
-      { title: "Blog Details", link: "/single-blog" },
-      { title: "service", link: "/service" },
-      { title: "service details", link: "/single-service" },
-      { title: "team", link: "/team" },
-      { title: "team details", link: "/single-team" },
-      { title: "faq", link: "/faq" },
-      { title: "errors 404", link: "/errors-404" }, 
-    ],
+    title: "About Us",
+    has_dropdown: false,
+    link: "/about-us",
   },
   {
-    title: "Portfolio",
-    has_dropdown: true,
-    link: "#",
-    submenu: [
-      { title: "Portfolio", link: "/portfolio" },
-      { title: "Portfolio Details", link: "/single-portfolio" },
-    ],
+    title: "Services",
+    has_dropdown: false,
+    link: "/service",
   },
   {
-    title: "Blog",
-    has_dropdown: true,
-    link: "#",
-    submenu: [
-      { title: "Blog", link: "/blog" },
-      { title: "Blog Grid", link: "/blog-grid" },
-      { title: "Blog details", link: "/single-blog" },
-    ],
-  },
-  {
-    title: "Contact Us",
+    title: "Contact",
     has_dropdown: false,
     link: "/contact-us",
   },
 ];
 
-
-
-
-const OffCanvas = ({setOpenMenu, openMenu} : any) => {
-
-    const [navTitle, setNavTitle] = useState("");
+const OffCanvas = ({ setOpenMenu, openMenu }: any) => {
+  const [navTitle, setNavTitle] = useState("");
   //openMobileMenu
   const openMobileMenu = (menu: string) => {
     if (navTitle === menu) {
@@ -72,10 +35,11 @@ const OffCanvas = ({setOpenMenu, openMenu} : any) => {
     }
   };
 
-
   return (
     <>
-      <div className={`techin-menu-wrapper ${openMenu ? "techin-body-visible" : ""}`}>
+      <div
+        className={`techin-menu-wrapper ${openMenu ? "techin-body-visible" : ""}`}
+      >
         <div className="techin-menu-area text-center">
           <div className="techin-menu-mobile-top">
             <div className="mobile-logo">
@@ -83,33 +47,62 @@ const OffCanvas = ({setOpenMenu, openMenu} : any) => {
                 <img src="assets/images/logo/logo1.svg" alt="logo" />
               </Link>
             </div>
-            <button className="techin-menu-toggle mobile" onClick={() => setOpenMenu(false)}>
+            <button
+              className="techin-menu-toggle mobile"
+              onClick={() => setOpenMenu(false)}
+            >
               <i className="ri-close-line"></i>
             </button>
           </div>
           <div className="techin-mobile-menu">
             <ul>
               {mobile_menu_data.map((item, i) => (
-                <li key={i} className={`menu-item-has-submenu menu-item-has-children techin-item-has-children ${navTitle === item.title ? "techin-active" : ""}`}>
-                  <Link to={item.link}>{item.title}
-                  {item.has_dropdown ? <span className="techin-mean-expand" onClick={() => openMobileMenu(item.title)}></span> : null} 
+                <li
+                  key={i}
+                  className={`menu-item-has-submenu menu-item-has-children techin-item-has-children ${navTitle === item.title ? "techin-active" : ""}`}
+                >
+                  <Link to={item.link}>
+                    {item.title}
+                    {item.has_dropdown ? (
+                      <span
+                        className="techin-mean-expand"
+                        onClick={() => openMobileMenu(item.title)}
+                      ></span>
+                    ) : null}
                   </Link>
                   {item.has_dropdown && (
-                    <ul className={`sub-menu techin-submenu ${navTitle === item.title ? "techin-open" : ""}`} style={{ display: navTitle === item.title ? "block" : "none", }}>
+                    <ul
+                      className={`sub-menu techin-submenu ${navTitle === item.title ? "techin-open" : ""}`}
+                      style={{
+                        display: navTitle === item.title ? "block" : "none",
+                      }}
+                    >
                       {item.submenu?.map((submenu, i) => (
                         <li key={i}>
                           <Link to={submenu.link}>{submenu.title}</Link>
-                        </li>                        
-                      ))} 
-                    </ul>                    
+                        </li>
+                      ))}
+                    </ul>
                   )}
                 </li>
-              ))} 
+              ))}
             </ul>
           </div>
           <div className="techin-mobile-menu-btn">
-            <Link className="techin-default-btn sm-size" to="/contact-us" data-text="Get in Touch"><span className="btn-wraper">Get in Touch</span></Link>
-            <Link className="techin-default-btn sm-size" to="/contact-us" data-text="Get in Touch"><span className="btn-wraper">Get in Touch</span></Link>
+            <Link
+              className="techin-default-btn sm-size"
+              to="/contact-us"
+              data-text="Get in Touch"
+            >
+              <span className="btn-wraper">Get in Touch</span>
+            </Link>
+            <Link
+              className="techin-default-btn sm-size"
+              to="/contact-us"
+              data-text="Get in Touch"
+            >
+              <span className="btn-wraper">Get in Touch</span>
+            </Link>
           </div>
         </div>
       </div>
